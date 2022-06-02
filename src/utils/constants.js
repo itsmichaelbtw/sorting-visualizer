66b15d1656b88c408e407ed52299488fda3f5b22
Michael <me@miikey.pw>
2022-06-02 14:22:50 +1000

    app constants

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const ViewModes = ["BarChart", "Box", "Mirror", "Compare"];
+export const DefaultView = "BarChart";
+export const InitialArraySize = 100;
+export const MaxArraySize = 2000;
+export const MinArraySize = 1;
+export const MaxNodeHeight = 100;
+export const MinNodeHeight = 5;
+export const AnimationSpeed = 5;
