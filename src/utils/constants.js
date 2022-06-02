324cd162eb7ee24a1bc6c346b28fab1d93b9b336
Michael <me@miikey.pw>
2022-06-02 23:26:48 +1000

    updated constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,13 @@
-export const ViewModes = ["BarChart", "Box", "Mirror", "Compare"];
-export const DefaultView = "BarChart";
 export const InitialArraySize = 100;
 export const MaxArraySize = 2000;
 export const MinArraySize = 1;
 export const MaxNodeHeight = 100;
 export const MinNodeHeight = 5;
-export const AnimationSpeed = 5;
+export const DefaultAnimationSpeed = "Normal";
+export const DefaultAlgorithm = "BubbleSort";
+export const DefaultView = "BarChart";
+
+export const ViewModes = ["BarChart", "Box", "Mirror", "Compare"];
+export const AnimationSpeeds = ["Slow", "Normal", "Fast"];
+export const Algorithms = ["BubbleSort", "SelectionSort", "InsertionSort", "MergeSort", "QuickSort", "HeapSort", "CountingSort", "RadixSort", "BucketSort", "ShellSort"];
+
