f19491aa09d2d22c0702fb0f9d5abede86e80afb
Michael <me@miikey.pw>
2022-06-02 23:24:46 +1000

    store provider and classname changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from "react";
 import Toolbar from "components/Toolbar";
 import Window from "components/Window";
 
+import { StoreProvider } from "context/store.context";
+
 export default function App() {
     return (
         <div className="h-full w-full">
-            <div className="h-full w-full flex flex-col">
-                <Toolbar />
-                <Window />
+            <div className="h-full w-full flex flex-col md:pr-[16px] md:pl-[16px] md:pb-[16px] min-w-[620px] max-w-[1920px]">
+                <StoreProvider>
+                    <Toolbar />
+                    <Window />
+                </StoreProvider>
             </div>
         </div>
     );
