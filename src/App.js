7b5a9f45de47cd242968727b1f6f579d50578e0d
Michael <me@miikey.pw>
2022-06-02 14:23:15 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import Toolbar from "components/Toolbar";
+import Window from "components/Window";
+
 export default function App() {
     return (
-        <p>Hello word</p>
+        <div className="h-full w-full">
+            <div className="h-full w-full flex flex-col">
+                <Toolbar />
+                <Window />
+            </div>
+        </div>
     );
 };
