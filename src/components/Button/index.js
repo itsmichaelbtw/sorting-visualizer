27f7089190e11f48892171854d14ee2f305c99d7
Michael <me@miikey.pw>
2022-06-02 23:25:00 +1000

    removed

--- src/components/Button/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Button({ onClick, children }) {
-    return (
-        <div className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded cursor-pointer ml-[4px] active:border-none active:mt-1 select-none">
-            {children}
-        </div>
-    );
-};
