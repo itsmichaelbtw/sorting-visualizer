150a613e5fca87b1da2e73a56e6321df339270da
Michael <me@miikey.pw>
2022-06-02 23:25:45 +1000

    added view changes with context store

--- src/components/Window/index.js
+++ src/components/Window/index.js
@@ -1,27 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import { generateArray } from "utils/array";
-import { InitialArraySize } from "utils/constants";
+import BarChart from "./Views/BarChart";
+import Mirror from "./Views/Mirror";
+
+import { useStore } from "context/store.context";
 
 export default function Window () {
-    const [array, setArray] = useState([]);
-
-    useEffect(() => {
-        const array = generateArray(InitialArraySize);
-        setArray(array);
-
-        console.log(array)
-    }, []);
+    const { currentArray, currentView, dispatch } = useStore();
 
     return (
-        <div className="h-full w-full flex max-w-[1920px] ml-auto mr-auto md:pr-[32px] md:pl-[32px] md:pb-[32px]">
-            <div className="flex-1 flex items-center bg-[#34495e] shadow-md rounded p-[16px] min-w-[620px]">
+        <div className="h-full w-full flex ml-auto mr-auto">
+            <div className="flex-1 bg-[#34495e] shadow-md rounded p-[8px]">
                 {
-                    array.map((node, index) => (
-                        <div key={index} className="w-full bg-green-400 rounded node" style={{ height: node.height }} />
-                    ))
+                    currentView === "BarChart" && <BarChart />
+                }
+                {
+                    currentView === "Mirror" && <Mirror />
                 }
             </div>
+            {/* <div className="flex-1 flex items-center bg-[#34495e] shadow-md rounded p-[16px] min-w-[620px]">
+                {
+                    currentArray.map((node, index) => (
+                        <div key={index} className="w-full bg-green-400" style={{ height: node.height }} />
+                    ))
+                }
+            </div> */}
         </div>
     );
 };
