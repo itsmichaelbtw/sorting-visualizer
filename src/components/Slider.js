b7b38277fcccca308ebe1136fe9f6ac54ac2f80a
Michael <me@miikey.pw>
2022-06-02 23:25:15 +1000

    slider component

---
+++ src/components/Slider.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactSlider from "react-slider";
+
+import { MinArraySize, MaxArraySize, InitialArraySize } from "utils/constants";
+
+export default function Slider({ min = MinArraySize, max = MaxArraySize, value = InitialArraySize, onChange }) {
+    return (
+        <div className="w-full h-[40px] flex flex-row rounded bg-slate-700 shadow-md">
+            <div className="py-2 px-4 bg-slate-800 rounded min-w-[76px] text-center select-none">
+                <span className="font-bold text-white leading-[24px] text-lg">Size</span>
+            </div>
+            <div className="py-2 px-4 w-full flex items-center min-w-[240px]">
+                <ReactSlider
+                    className="w-full h-[4px] rounded bg-slate-800"
+                    thumbClassName="h-[18px] w-[18px] bg-blue-400 border-2 border-white hover:bg-blue-500 rounded-full -top-[7px] shadow-sm cursor-pointer focus:outline-none"
+                    trackClassName="track"
+                    defaultValue={value}
+                    min={min}
+                    max={max}
+                    onChange={onChange}
+                />
+            </div>
+            <div className="py-2 px-4 bg-slate-800 rounded min-w-[76px] text-center select-none">
+                <span className="font-bold text-white leading-[24px] text-lg">{value}</span>
+            </div>
+        </div>
+    );
+}
