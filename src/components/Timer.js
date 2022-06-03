d02078800135190b9a9e37fc7044c86cc25bd1c4
Michael <me@miikey.pw>
2022-06-03 21:40:59 +1000

    timer functionality

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Timer({ prefix = null, suffix = null }) {
+import { TimerFrameRate } from "utils/constants";
+
+export default function Timer({ onPause, onStart, children }) {
+    const now = new Date().getTime();
+
+    const [run, setRun] = useState(false);
+    const [deltaTime, setDelta] = useState(0);
+
+    useEffect(() => {
+        let timer = null;
+
+        if (run) {
+            timer = setInterval(() => {
+                setDelta(new Date().getTime() - now);
+            }, TimerFrameRate);
+        };
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, [run]);
+
+    const onClick = () => {
+        if (run) {
+            onPause();
+        } else {
+            onStart(() => {
+                setRun(false);
+            });
+        };
+
+        setRun((v) => !v);
+    };
+
+    const formatTime = (time) => {
+        const ms = time % 1000;
+        const s = Math.floor(time / 1000) % 60;
+        const m = Math.floor(time / 60000);
+
+        return `${m.toString().padStart(2, "0")}:${s.toString().padStart(2, "0")}:${ms.toString().padStart(3, "0")}`;
+    };
+
     return (
         <div className="w-full h-[40px] flex flex-row rounded bg-slate-700 shadow-md select-none">
-            {prefix}
+            {children({ onClick })}
             <div className="py-2 px-4 w-full flex items-center">
-                <span className="font-bold text-white text-lg">00:00</span>
+                <span className="font-bold text-white text-lg w-[83px]">
+                    {formatTime(deltaTime)}
+                </span>
             </div>
-            {suffix}
         </div>
     );
 };
