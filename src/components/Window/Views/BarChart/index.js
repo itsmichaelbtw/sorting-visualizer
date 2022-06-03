57a9defec0c77360246648054308398ae176469b
Michael <me@miikey.pw>
2022-06-03 21:42:13 +1000

    prop updates

--- src/components/Window/Views/BarChart/index.js
+++ src/components/Window/Views/BarChart/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-import Node from "./Node";
+import Node from "../../Node";
 
 import { renderArray } from "utils/array";
 import { useStore } from "context/store.context";
 
 export default function BarChart() {
-    const { currentArray } = useStore();
+    const { currentArray, currentView, windowRef } = useStore();
 
     return (
-        <div className="h-full w-full flex flex-row items-end rounded overflow-hidden">
-            {renderArray(currentArray, (item, index) => {
-                return <Node key={index} {...item} />;
+        <div className="h-full w-full flex flex-row items-end rounded overflow-hidden" ref={windowRef}>
+            {renderArray(currentArray, (node, index) => {
+                return <Node key={index} node={node} currentView={currentView} />;
             })}
         </div>
     );
--- src/components/Window/Views/Mirror/index.js
+++ src/components/Window/Views/Mirror/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-import Node from "./Node";
+import Node from "../../Node";
 
-import { renderArray } from "utils/array";
+import { renderArray, calculateMarginalGap } from "utils/array";
 import { useStore } from "context/store.context";
 
 export default function Mirror() {
-    const { currentArray } = useStore();
+    const { currentArray, currentView } = useStore();
 
     return (
         <div className="h-full w-full flex items-center">
-            {renderArray(currentArray, (item, index) => {
-                return <Node key={index} {...item} />;
+            {renderArray(currentArray, (node, index) => {
+                return <Node key={index} node={node} currentView={currentView} />;
             })}
         </div>
     );
