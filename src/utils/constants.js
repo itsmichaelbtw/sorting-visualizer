3327f0fe940a92de8a53f1de23166bf32aed4b87
Michael <me@miikey.pw>
2022-06-03 21:42:53 +1000

    constants updates

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,12 @@
-export const InitialArraySize = 100;
-export const MaxArraySize = 2000;
-export const MinArraySize = 1;
+export const InitialArraySize = 50;
+export const MaxArraySize = 200;
+export const MinArraySize = 5;
 export const MaxNodeHeight = 100;
 export const MinNodeHeight = 5;
+export const TimerFrameRate = 10;
 export const DefaultAnimationSpeed = "Normal";
 export const DefaultAlgorithm = "BubbleSort";
-export const DefaultView = "BarChart";
+export const DefaultView = "Mirror";
 
 export const ViewModes = ["BarChart", "Box", "Mirror", "Compare"];
 export const AnimationSpeeds = ["Slow", "Normal", "Fast"];
