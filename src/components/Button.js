a6938bdc67c066116dd6fd2d711500cc6142b650
Michael <me@miikey.pw>
2022-06-03 21:40:43 +1000

    classname updates + disabled prop

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Button({ suffix, prefix, children, ...props }) {
+export default function Button({ suffix, prefix, disabled, children, ...props }) {
     return (
         <div 
             {...props} 
-            className="bg-blue-500 h-[40px] hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded cursor-pointer active:border-none active:mt-1 select-none shadow-md min-w-fit">
+            className={`bg-blue-500 h-[40px] hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded select-none shadow-md ${disabled ? "cursor-not-allowed" : "cursor-pointer active:border-none active:mt-1"}`}>
             {
                 prefix && (
                     <span>
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,7 +3,7 @@ import ReactSlider from "react-slider";
 
 import { MinArraySize, MaxArraySize, InitialArraySize } from "utils/constants";
 
-export default function Slider({ min = MinArraySize, max = MaxArraySize, value = InitialArraySize, onChange }) {
+export default function Slider({ min = MinArraySize, max = MaxArraySize, value = InitialArraySize, disabled, onChange }) {
     return (
         <div className="w-full h-[40px] flex flex-row rounded bg-slate-700 shadow-md">
             <div className="py-2 px-4 bg-slate-800 rounded min-w-[76px] text-center select-none">
@@ -12,12 +12,13 @@ export default function Slider({ min = MinArraySize, max = MaxArraySize, value =
             <div className="py-2 px-4 w-full flex items-center min-w-[240px]">
                 <ReactSlider
                     className="w-full h-[4px] rounded bg-slate-800"
-                    thumbClassName="h-[18px] w-[18px] bg-blue-400 border-2 border-white hover:bg-blue-500 rounded-full -top-[7px] shadow-sm cursor-pointer focus:outline-none"
+                    thumbClassName={`h-[18px] w-[18px] bg-blue-400 border-2 border-white hover:bg-blue-500 rounded-full -top-[7px] shadow-sm focus:outline-none ${disabled ? "cursor-not-allowed" : "cursor-pointer"}`}
                     trackClassName="track"
                     defaultValue={value}
                     min={min}
                     max={max}
                     onChange={onChange}
+                    disabled={disabled}
                 />
             </div>
             <div className="py-2 px-4 bg-slate-800 rounded min-w-[76px] text-center select-none">
